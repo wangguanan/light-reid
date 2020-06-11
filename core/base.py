fix precision-recall bug

core/base.py
@@ -44,14 +44,13 @@ class Base:
 
 
 	def _init_model(self):
+		pretrained = False if self.config.mode != 'train' else True
 		if self.cnnbackbone == 'res50':
-			self.model = Res50BNNeck(class_num=self.pid_num)
-			self.model = nn.DataParallel(self.model).to(self.device)
+			self.model = Res50BNNeck(class_num=self.pid_num, pretrained=pretrained).to(self.device)
 		elif self.cnnbackbone == 'res50ibna':
-			self.model = Res50IBNaBNNeck(class_num=self.pid_num)
-			self.model = nn.DataParallel(self.model).to(self.device)
+			self.model = Res50IBNaBNNeck(class_num=self.pid_num, pretrained=pretrained).to(self.device)
 		elif self.cnnbackbone == 'osnetain':
-			self.model = osnet_ain_x1_0(num_classes=self.pid_num, pretrained=True, loss='softmax')
+			self.model = osnet_ain_x1_0(num_classes=self.pid_num, pretrained=pretrained, loss='softmax')
 		else:
 			assert 0, 'cnnbackbone error, expect res50, res50ibna, osnetain'
 		self.model = self.model.to(self.device)

core/nets/res50bnneck.py
@@ -5,12 +5,12 @@ from .bnneck import BNClassifier
 
 class Res50BNNeck(nn.Module):
 
-    def __init__(self, class_num):
+    def __init__(self, class_num, pretrained=True):
         super(Res50BNNeck, self).__init__()
 
         self.class_num = class_num
         # backbone and optimize its architecture
-        resnet = torchvision.models.resnet50(pretrained=True)
+        resnet = torchvision.models.resnet50(pretrained=pretrained)
         resnet.layer4[0].conv2.stride = (1,1)
         resnet.layer4[0].downsample[0].stride = (1,1)
 

core/nets/res50ibnabnneck.py
@@ -222,12 +222,12 @@ def resnet152_ibn_a(pretrained=False, **kwargs):
 
 class Res50IBNaBNNeck(nn.Module):
 
-    def __init__(self, class_num):
+    def __init__(self, class_num, pretrained=True):
         super(Res50IBNaBNNeck, self).__init__()
 
         self.class_num = class_num
         # backbone and optimize its architecture
-        resnet = resnet50_ibn_a(pretrained=True)
+        resnet = resnet50_ibn_a(pretrained=pretrained)
         resnet.layer4[0].conv2.stride = (1, 1)
         resnet.layer4[0].downsample[0].stride = (1, 1)
 

core/test.py
@@ -1,6 +1,8 @@
 import torch
-from tools import time_now, CatMeter, ReIDEvaluator
-
+from tools import time_now, CatMeter, ReIDEvaluator, PrecisionRecall
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 def test(config, base, loaders):
 
@@ -56,6 +58,23 @@ def test(config, base, loaders):
 		query_features, query_cids_meter.get_val_numpy(), query_pids_meter.get_val_numpy(),
 		gallery_features, gallery_cids_meter.get_val_numpy(), gallery_pids_meter.get_val_numpy())
 
-	return mAP, CMC[0: 150]
+	# compute precision-recall curve
+	thresholds = np.linspace(1.0, 0.0, num=101)
+	pres, recalls, thresholds = PrecisionRecall(dist='cosine', mode=config.test_mode).evaluate(
+		thresholds, query_features, query_cids_meter.get_val_numpy(), query_pids_meter.get_val_numpy(),
+		gallery_features, gallery_cids_meter.get_val_numpy(), gallery_pids_meter.get_val_numpy())
 
+	return mAP, CMC[0: 150], pres, recalls, thresholds
+
+
+def plot_prerecall_curve(config, pres, recalls, thresholds, mAP, CMC, label):
+
+	plt.plot(recalls, pres, label='{model},map:{map},cmc135:{cmc}'.format(
+		model=label, map=round(mAP, 2), cmc=[round(CMC[0], 2), round(CMC[2], 2), round(CMC[4], 2)]))
+	plt.xlabel('recall')
+	plt.ylabel('precision')
+	plt.title('precision-recall curve')
+	plt.legend()
+	plt.grid()
+	plt.savefig(os.path.join(config.output_path, 'precisio-recall-curve.png'))
 