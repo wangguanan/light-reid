3c45a7d3be03abc1e2e145cdd9746bb67b0ad0b8
wangguanan <guan.wang0706@gmail.com>
2020-11-06 18:40:45 +0800

    add example bagtricks_buildwithconfigs, more simple

---
+++ examples/bagtricks_buildwithconfigs/train.py
@@ -0,0 +1,24 @@
+import argparse, ast
+import sys
+sys.path.append('../..')
+sys.path.append('.')
+import torch
+import lightreid
+import yaml
+import time
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config_file', type=str, default='./configs/base_config.yaml', help='')
+args = parser.parse_args()
+
+# init configs
+with open(args.config_file) as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+solver = lightreid.build_engine(config)
+
+# train
+solver.train(eval_freq=10)
+# test
+solver.resume_latest_model()
+solver.smart_eval(onebyone=True, mode='normal')
